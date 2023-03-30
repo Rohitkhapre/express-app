505b0359c99d2500015ceb31a0fce491ef633f9e
Rohit khapre <rohitkhapre7@gmail.com>
2023-03-30 13:15:16 +0530

    update the provider.tf file for the error

--- provider.tf
+++ provider.tf
@@ -4,7 +4,5 @@
 # Specify the provider and access details
 provider "aws" {
   profile = "default"
-  access_key = "${secrets.AWS_ACCESS_KEY}"
-  secret_key = "${secrets.AWS_SECRET_ACCESS_KEY}"
   region  = var.aws_region
 }
