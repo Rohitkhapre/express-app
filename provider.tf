f60987e37d9243d7f75a93d23696145ac32c8e24
Rohit khapre <rohitkhapre7@gmail.com>
2023-03-31 16:12:55 +0530

    add terraform workspace

--- provider.tf
+++ provider.tf
@@ -2,6 +2,25 @@
 # provider.tf
 
 # Specify the provider and access details
+#provider "aws" {
+#  region = var.aws_region
+#}
+
+terraform {
+  cloud {
+    organization = "rohitpawar27"
+    workspaces {
+      name = "demo-ecs"
+    }
+  }
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
 provider "aws" {
   region = var.aws_region
+  # profile = var.aws_profile
 }
