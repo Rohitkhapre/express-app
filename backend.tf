009322891d65fa5db290f9b6b3da82f899cc3d6b
Rohit khapre <rohitkhapre7@gmail.com>
2023-03-31 18:29:54 +0530

    add credential for s3

--- backend.tf
+++ backend.tf
@@ -3,5 +3,8 @@ terraform {
     bucket = "my-terraform-state-bucket"
     key    = "terraform.tfstate"
     region = "us-east-1"
+    access_key = var.aws_access_key
+    secret_key = var.aws_secret_key
+    dynamodb_table = "terraform-lock-table"
   }
 }
